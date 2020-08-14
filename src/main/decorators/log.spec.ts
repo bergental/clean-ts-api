b558b757e98d3d8e951039ceae203fa406f37991
Pedro Henrique Bergental <phbergental@gmail.com>
2020-08-14 01:41:17 -0300

    refactor: move HttpHelper to a folder

--- src/main/decorators/log.spec.ts
+++ src/main/decorators/log.spec.ts
@@ -1,9 +1,8 @@
 import { LogControllerDecorator } from './log'
 import { Controller, HttpRequest, HttpResponse } from '../../presentation/protocols'
-import { serverError } from '../../presentation/helpers/http-helper'
+import { serverError, ok } from '../../presentation/helpers/http/http-helper'
 import { LogErrorRepository } from '../../data/protocols/log-error-repository'
 import { AccountModel } from '../../domain/models/account'
-import { ok } from './../../presentation/helpers/http-helper'
 
 interface SutTypes {
   sut: LogControllerDecorator
