1f63037a62267277b7b69907acd472f7f3d99de8
Pedro H. Bergental <phbergental@gmail.com>
2021-05-19 01:06:45 -0300

    refactor: rename folder usercases for usecases

--- src/main/factories/controllers/signup/signup-controller-factory.ts
+++ src/main/factories/controllers/signup/signup-controller-factory.ts
@@ -1,8 +1,8 @@
 import { Controller } from '../../../../presentation/protocols'
 import { SignUpController } from '../../../../presentation/controller/login/signup/signup-controller'
 import { makeSignUpValidation } from './signup-validation-factory'
-import { makeDbAuthentication } from '../../usercases/authentication/db-authentication-factory'
-import { makeDbAddAccount } from '../../usercases/add-account/db-add-account-factory'
+import { makeDbAuthentication } from '../../usecases/authentication/db-authentication-factory'
+import { makeDbAddAccount } from '../../usecases/add-account/db-add-account-factory'
 import { makeLogControllerDecorator } from '../../decorators/log-controller-decorator-factory'
 
 export const makeSignUpController = (): Controller => {
