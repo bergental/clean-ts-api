2c2da62a9babfe525043977a261395180b818ee5
Pedro H. Bergental <phbergental@gmail.com>
2021-07-11 09:50:53 -0300

    refactor: move request creation to a factory method

--- src/presentation/middlewares/auth-middleware.ts
+++ src/presentation/middlewares/auth-middleware.ts
@@ -1,4 +1,4 @@
-import { LoadAccountByToken } from '../../domain/usecases/load-account by-token'
+import { LoadAccountByToken } from '../../domain/usecases/load-account-by-token'
 import { AccessDeniedError } from '../errors/access-denied-error'
 import { forbidden } from '../helpers/http/http-helper'
 import { Middleware, HttpRequest, HttpResponse } from '../protocols'
