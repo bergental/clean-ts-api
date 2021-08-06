e70e92fd25864c8aebfae9e83e42ad6617890bd3
Pedro H. Bergental <phbergental@gmail.com>
2021-08-05 21:28:30 -0300

    feat: ensure load surveys route returns 403 if no access token is provided

---
+++ src/main/factories/controllers/survey/load-surveys/load-surveys-controller-factory.ts
@@ -0,0 +1,9 @@
+import { Controller } from '../../../../../presentation/protocols'
+import { LoadSurveyController } from '../../../../../presentation/controller/survey/load-surveys/load-surveys-controller'
+import { makeLogControllerDecorator } from '../../../decorators/log-controller-decorator-factory'
+import { makeDbLoadSurveys } from '../../../usecases/survey/load-surveys/db-load-surveys-factory'
+
+export const makeLoadSurveysController = (): Controller => {
+  const controller = new LoadSurveyController(makeDbLoadSurveys())
+  return makeLogControllerDecorator(controller)
+}
