fafe5749d1d8caf581a61256d00474bb350d6444
Pedro H. Bergental <phbergental@gmail.com>
2021-10-29 01:17:37 -0300

    chore: release 2.3.0

--- src/main/docs/index.ts
+++ src/main/docs/index.ts
@@ -7,7 +7,7 @@ export default {
   info: {
     title: 'Clean Node API',
     description: 'API do curso do Mango para realizar enquetes entre programadores',
-    version: '2.2.0'
+    version: '2.3.0'
   },
   license: {
     name: 'GPL-3.0-or-later',
